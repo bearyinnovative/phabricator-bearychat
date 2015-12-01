13bf96c296061634ccc66b3d494354cd94093dea
zjhmale <zjhmale@gmail.com>
2015-12-01 14:16:28 +0800

    done

--- app/routes.js
+++ app/routes.js
@@ -1,4 +1,7 @@
 var config = require('config');
+var querystring = require('querystring');
+var request = require('request');
+var superagent = require('superagent');
 
 module.exports = function(app, router) {
 	router.use(function(req, res, next) {
@@ -8,24 +11,63 @@ module.exports = function(app, router) {
 
 	var index = router.route("/");
 
-	index.post(function(request, response) {
+	index.post(function(req, res) {
+		res.send('OK');
+
 		if (req.body.storyData && req.body.storyData.objectPHID) {
+			var token = config.get("phabricator.token");
+			var queryurl = config.get("phabricator.api") + "/api/phid.query";
+			var sendurl = config.get("bearychat.url");
+			var phid = req.body.storyData.objectPHID;
+			var text = req.body.storyText;
 
-		}
-	});
+			var form = {
+				'api.token': token,
+				'phids[]': phid
+			};
 
-	app.use('/', router);
+			var formData = querystring.stringify(form);
+			var contentLength = formData.length;
 
-	app.use(function(req, res, next) {
-		res.status(404);
+			request({
+				headers: {
+					'Content-Length': contentLength,
+					'Content-Type': 'application/x-www-form-urlencoded'
+				},
+				uri: queryurl,
+				body: formData,
+				method: 'POST'
+			}, function(err, res, body) {
+				if (!err) {
+					var sendbody = JSON.parse(body);
+					sendbody.result[phid].text = text;
 
-		if (req.accepts('json')) {
-			res.send({
-				error: 'Not found'
+					superagent
+					.post(sendurl)
+					.send(sendbody)
+					.set('Accept', 'application/json')
+					.end(function(err, res) {
+						if (!err) {
+							console.log(res);
+						};
+					});
+				};
 			});
-			return;
 		}
-
-		res.type('txt').send('Not found');
 	});
+
+app.use('/', router);
+
+app.use(function(req, res, next) {
+	res.status(404);
+
+	if (req.accepts('json')) {
+		res.send({
+			error: 'Not found'
+		});
+		return;
+	}
+
+	res.type('txt').send('Not found');
+});
 }
